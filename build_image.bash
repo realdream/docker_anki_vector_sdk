feffe1df9749b0c14a45332b9cffae46015cd5aa
Renkai Ye <realdream2016@outlook.com>
2020-05-29 22:00:06 +0800

    change to ubt 18.04

--- build_image.bash
+++ build_image.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build docker/ubt16_sdk -t ubuntu:anki_sdk
+docker build ubt18_sdk -t anki_sdk:18.04 --build-arg http_proxy="http://172.17.0.1:8123"  --build-arg https_proxy="http://172.17.0.1:8123"
--- start_container.bash
+++ start_container.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 xhost +
 docker run -it \
--v /home/Anki/vector-python-sdk:/home/user/Anki \
--v /home/Anki/anki_vector_usr_dir:/home/user/.anki_vector \
+-v /home/Anki:/root/Anki \
+-v /home/Anki/anki_vector_usr_dir:/root/.anki_vector \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=unix$DISPLAY \
 -e GDK_SCALE \
 -e GDK_DPI_SCALE \
-ubuntu:anki_sdk
+--name anki_vector_sdk \
+anki_sdk:18.04
